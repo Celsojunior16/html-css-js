59fac38be6752eaf6f0c4caef564826c83dc5ca8
Celsojunior16 <alvarelacjunior@gmail.com>
2024-10-31 14:53:33 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+let nome = prompt("Digite seu nome...")
+let sobrenome = prompt ("Digite seu sobrenome.....")
+
+console.log("Nome Completo: " + nome + " " + sobrenome)
+
+
+document.getElementById('resultado').innerHTML =  "Nome completo:" +  " "  + nome + " "+ sobrenome;
